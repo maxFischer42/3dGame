918772ff9eca10b5cddfcdc61c1d08f9795a57f5
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-04-10 13:59:02 -0700

    Enemy AI base

---
+++ 3D_Game/Assets/Debug Scenes/EnemyFootsteps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFootsteps : MonoBehaviour {
+	
+	public AudioSource audi;
+	public AudioClip[] footSteps;
+	public float timeBetweenSteps;
+	float timer;
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GetComponent<Rigidbody> ().velocity.x != 0 || GetComponent<Rigidbody> ().velocity.z != 0) {
+			timer += Time.deltaTime;
+			if (timer >= timeBetweenSteps) {
+				int soundToPlay = Random.Range (0, footSteps.Length);
+				audi.PlayOneShot (footSteps [soundToPlay]);
+				timer = 0;
+			}
+		}
+	}
+}
---
+++ 3D_Game/Assets/Debug Scenes/EnemyNavFollow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavFollow : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform player;
+	public Transform[] points;
+	public bool isChasingPoint;
+	public float lookSpeed;
+	public bool priorityPlayer;
+    NavMeshAgent agent;
+
+	void Start ()
+	{
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!priorityPlayer) {
+			if (!isChasingPoint) {
+				player = points [Random.Range (0, points.Length)];
+				isChasingPoint = true;
+			}
+		}
+		if (priorityPlayer) {
+			player = GameObject.Find ("Player").GetComponent<Transform> ();
+		}
+		Vector3 targetDir = player.position - transform.position;
+		float step = lookSpeed * Time.deltaTime;
+		Vector3 newDir = Vector3.RotateTowards (transform.forward, targetDir, step, 0.0f);
+
+        agent.destination = player.position;
+	}
+
+	void OnTriggerEnter(Collider Coll)
+	{
+		if (Coll.transform == player) {
+			isChasingPoint = false;
+		} else if (Coll.transform.name == "PlayerVisionCone" || Coll.transform.name == "footStepRadius") {
+			priorityPlayer = true;
+		}
+	}
+
+	void OnTriggerExit(Collider Coll)
+	{
+		if (Coll.transform.name == "footStepRadius") {
+			priorityPlayer = false;
+		}
+	}
+}
