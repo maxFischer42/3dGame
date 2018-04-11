726b7f03494f4c56a7c09f3e5c141e7efd3178ca
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-04-11 12:42:33 -0700

    Fixed bugs from editor

---
+++ 3dProj/Assets/Scripts/Player/playerShoot.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class playerShoot: MonoBehaviour {
+
+	public int gunDamage = 1;                                           
+
+	public float fireRate = .25f;                                       
+
+	public float weaponRange = 50f;                                     
+
+	public float hitForce = 100f;
+
+	public Transform gunEnd;
+
+
+
+
+
+	private Camera fpsCam;
+
+	private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+
+	private AudioSource gunAudio;
+
+	private LineRenderer laserLine;
+
+	private float nextFire; 
+
+
+
+	void Start () 
+	{
+		laserLine = GetComponent<LineRenderer>();
+		gunAudio = GetComponent<AudioSource>();
+		fpsCam = GetComponentInParent<Camera> ();
+	}
+
+	void Update () 
+	{
+
+		if (Input.GetButtonDown ("Fire1") && Time.time > nextFire) 
+
+		{
+
+			nextFire = Time.time + fireRate;
+			StartCoroutine(ShotEffect());
+			Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (.5f, .5f, 0));
+			RaycastHit hit;
+			laserLine.SetPosition(0, gunEnd.position);
+			if (Physics.Raycast(rayOrigin,fpsCam.transform.forward, out hit, weaponRange))
+			{
+				laserLine.SetPosition (1, hit.point);
+			}
+			else
+
+			{
+				laserLine.SetPosition(1, fpsCam.transform.forward * weaponRange);
+			}
+
+		}
+
+
+
+
+	}
+
+
+
+
+	private IEnumerator ShotEffect()
+
+	{
+
+		gunAudio.Play ();
+		laserLine.enabled = true;
+		yield return shotDuration;
+
+		laserLine.enabled = false;
+	}
+
+
+}
---
+++ 3dProj/Assets/disableAudioSources.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableAudioSources : MonoBehaviour {
+
+	void OnTriggerEnter(Collider Coll)
+	{
+		if (Coll.transform.gameObject.tag == "Enemy" && Coll.GetComponent<AudioSource>())
+		{
+			Coll.GetComponent<AudioSource> ().enabled = true;
+		}
+	}
+	void OnTriggerStay(Collider Coll)
+	{
+		if (Coll.transform.gameObject.tag == "Enemy" && Coll.GetComponent<AudioSource>())
+		{
+			Coll.GetComponent<AudioSource> ().enabled = true;
+		}
+	}
+	void OnTriggerExit(Collider Coll)
+	{
+		if (Coll.transform.gameObject.tag == "Enemy" && Coll.GetComponent<AudioSource>())
+		{
+			Coll.GetComponent<AudioSource> ().enabled = false;
+		}
+	}
+
+
+}
