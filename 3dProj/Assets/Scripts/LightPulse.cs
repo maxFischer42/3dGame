d0792f15dff1b5ec27283737b34091c93783761a
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-04-17 13:11:13 -0700

    Hands

--- 3dProj/Assets/Scripts/LightPulse.cs
+++ 3dProj/Assets/Scripts/LightPulse.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightPulse : MonoBehaviour {
+public class LightPulse : MonoBehaviour
+{
+    float intensity;
+    bool up = true;
+    public float range;
+    public float size;
+    // Use this for initialization
+    void Start()
+    {
+        intensity = gameObject.GetComponent<Light>().intensity;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.GetComponent<Light>().intensity <= intensity + range && gameObject.GetComponent<Light>().intensity >= intensity && up || gameObject.GetComponent<Light>().intensity == 0)
+        {
+
+            //gameObject.GetComponent<Light>().intensity += .1f;
+            up = true;
+        }
+        else if (gameObject.GetComponent<Light>().intensity >= intensity + range)
+        {
+            //gameObject.GetComponent<Light>().intensity -= .1f;
+            up = false;
+        }
+
+        if (up)
+        {
+            gameObject.GetComponent<Light>().intensity += size;
+        }
+        if (!up)
+        {
+            gameObject.GetComponent<Light>().intensity -= size;
+        }
+    }
 }
+
