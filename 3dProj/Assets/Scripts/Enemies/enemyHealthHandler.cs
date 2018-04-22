bcfd8469f614c026174f9ec1836c83b6d4efbee8
maxFischer42 <42ducksable1@gmail.com>
2018-04-22 14:18:32 -0700

    A

--- 3dProj/Assets/Scripts/Enemies/enemyHealthHandler.cs
+++ 3dProj/Assets/Scripts/Enemies/enemyHealthHandler.cs
@@ -6,15 +6,19 @@ public class enemyHealthHandler : MonoBehaviour {
 
 	public int maxHP;
 	public GameObject wreckage;
+	public bool isShield;
+	public shieldHealth shieldScript;
 
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (maxHP <= 0) {
-			GameObject wreck = (GameObject) Instantiate (wreckage, transform.position, Quaternion.identity);
+		if (maxHP <= 0 && !isShield) {
+			GameObject wreck = (GameObject)Instantiate (wreckage, transform.position, Quaternion.identity);
 			wreck.transform.parent = null;
 			Destroy (gameObject);
+		} else if (maxHP <= 0 && isShield) {
+			shieldScript.hp--;
 		}
 	}
 
---
+++ 3dProj/Assets/shieldHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shieldHealth : MonoBehaviour {
+
+	public GameObject part;
+	public int hp;
+	public GameObject[] newStuff;
+
+	public void Update()
+	{
+		if (hp == 0) {
+			explosion();
+		}
+	}
+
+	public void explosion()
+	{
+		part.SetActive (true);
+		for(int i = 0; i < newStuff.Length; i++)
+		{
+			newStuff[i].SetActive(true);
+		}
+
+	}
+
+
+}
