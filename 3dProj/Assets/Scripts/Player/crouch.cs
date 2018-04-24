c026d619392c82fd37ba88e4c82efb83625ec4b2
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-04-24 11:22:57 -0700

    h

---
+++ 3dProj/Assets/Scripts/Player/crouch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crouch : MonoBehaviour {
+    public bool debug;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(gameObject.transform.GetComponent<Rigidbody>().velocity.y);
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (gameObject.transform.localScale.z > 0.5f)
+                gameObject.transform.localScale.Set(gameObject.transform.localScale.x, gameObject.transform.localScale.y - 0.03f, gameObject.transform.localScale.z);
+        }
+        }
+}
