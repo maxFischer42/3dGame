daca91fb068386e88c1d9726fb6d7d5335649e38
maxFischer42 <42ducksable1@gmail.com>
2018-04-29 20:01:31 -0700

    AAA

---
+++ 3dProj/Assets/killscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class killscript : MonoBehaviour {
+
+	private float playerHp;
+	public playerHealthHandler hpScript;
+	public string sceneToLoad;
+
+
+	void Update () {
+		playerHp = hpScript.HP;
+		if (playerHp <= 0) {
+			SceneManager.LoadScene (sceneToLoad);
+		}
+	}
+
+
+}
