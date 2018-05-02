73cb05d9bb5abc5b3f86552ed242e7fd17733c37
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-05-02 12:43:17 -0700

    spam

---
+++ 3dProj/Assets/Scripts/SpawnOnPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnPickup : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject[] ok;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < ok.Length; i++)
+        {
+
+
+            GameObject Enemy = (GameObject)Instantiate(enemy, ok[i].transform.position, Quaternion.identity);
+        }
+    }
+}
--- 3dProj/Assets/spin.cs
+++ 3dProj/Assets/spin.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class spin : MonoBehaviour {
-
+    public bool move;
 	// Use this for initialization
 	void Start () {
         gameObject.GetComponent<Rigidbody>().AddTorque(Random.Range(0, 100), Random.Range(0, 100), Random.Range(0, 100));
-        gameObject.GetComponent<Rigidbody>().AddForce(Random.Range(0, 40), Random.Range(0, 40), Random.Range(0, 40));
+        if (move)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Random.Range(0, 40), Random.Range(0, 40), Random.Range(0, 40));
+        }
 
     }
 	
