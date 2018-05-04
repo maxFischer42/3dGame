5a5f59a1b14056a6072b66caa63b15c2498b4e62
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-05-04 11:53:14 -0700

    hi quit reading my text

---
+++ 3dProj/Assets/flyAway.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+public class flyAway : MonoBehaviour {
+    public GameObject sheild;
+    public string scenetoload;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == "Player" && sheild.GetComponent<shieldHealth>().hp == 0) 
+        {
+            SceneManager.LoadScene(scenetoload);        }
+    }
+}
